65950e05c0b7ed75fe0921660ef32cf648158628
Utkarsh-quytech <152372849+Utkarsh-quytech@users.noreply.github.com>
2024-05-07 10:26:35 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from datetime import datetime
-from pytz import timezone  # Import pytz library for handling timezones
+from pytz import timezone
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from gapps import CardService
@@ -17,26 +17,23 @@ async def root():
 async def homepage(gevent: models.GEvent):
     access_token = gevent.authorizationEventObject.userOAuthToken
     creds = Credentials(access_token)
-    page = send_reminder(creds)
+    page = send_reminder(creds, gevent.timezone)
     return page
 
-def send_reminder(creds):
-    unreplied_emails = get_unreplied_emails(creds)
+def send_reminder(creds, user_timezone):
+    unreplied_emails = get_unreplied_emails(creds, user_timezone)
     if unreplied_emails:
-        # Build the card for unreplied emails
         card = build_unreplied_emails_card(unreplied_emails)
         return card
     else:
-        # Return a card indicating no unreplied emails
         return CardService.newCardBuilder() \
             .setHeader(CardService.newCardHeader().setTitle('No Unreplied Emails')) \
             .build()
 
-def get_unreplied_emails(creds):
+def get_unreplied_emails(creds, user_timezone):
     unreplied_emails = []
     service = build('gmail', 'v1', credentials=creds)
 
-    # Get unreplied incoming emails
     threads = service.users().threads().list(userId='me', q='-is:chats -is:sent -is:draft -in:trash', maxResults=20).execute()
     if 'threads' in threads:
         for thread in threads['threads']:
@@ -50,8 +47,10 @@ def get_unreplied_emails(creds):
                 subject = [header['value'] for header in message_details['payload']['headers'] if header['name'] == 'Subject']
                 subject = subject[0] if subject else None
                 message_date = datetime.fromtimestamp(int(message_details['internalDate'])/1000.0)
-                message_date_with_timezone = message_date.replace(tzinfo=timezone('UTC')).astimezone(timezone('Asia/Kolkata')).strftime('%Y-%m-%d %H:%M:%S %Z')
-                # Check if the email is from the specified domain and not replied
+                
+                # Adjust timestamp to user's timezone
+                message_date_with_timezone = message_date.replace(tzinfo=timezone('UTC')).astimezone(timezone(user_timezone)).strftime('%Y-%m-%d %H:%M:%S %Z')
+                
                 if sender and '@quytech.com' in sender and not has_been_replied_to(service, thread_id):
                     unreplied_emails.append({'sender': sender, 'subject': subject, 'date': message_date_with_timezone})
     return unreplied_emails
@@ -63,7 +62,6 @@ def has_been_replied_to(service, thread_id):
 
 def build_unreplied_emails_card(emails):
     card = CardService.newCardBuilder().setHeader(CardService.newCardHeader().setTitle('Emails-Not-Replied'))
-    # Add sections for each unreplied email
     for email in emails:
         section = CardService.newCardSection() \
             .setHeader(email['subject']) \
